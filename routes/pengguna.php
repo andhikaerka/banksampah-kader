99246e21f215b9ff91c38bc5eac5bbf9a366a696
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-09-21 16:57:51 +0700

    Update pengguna bank sampah

--- routes/pengguna.php
+++ routes/pengguna.php
@@ -2,22 +2,20 @@
 
 use App\Http\Controllers\Pengguna\DashboardController;
 use App\Http\Controllers\Pengguna\Kader\KaderController;
+use App\Http\Controllers\Pengguna\ProfileController;
 use App\Http\Controllers\Pengguna\Setoran\SetoranController;
 
 Route::prefix('pengguna')->name('pengguna.')->group(function () {
 
-    // master bank sampah
-    Route::name('dashboard.')->group(function () {
-        Route::resource('dashboard', DashboardController::class);
-    });
+    // dashboard
+    Route::get('dashboard', DashboardController::class);
 
-    // master bank sampah
-    Route::name('kader.')->group(function () {
-        Route::resource('kader', KaderController::class);
-    });
+    // kader
+    Route::resource('kader', KaderController::class);
 
-    // master barang
-    Route::name('setoran.')->group(function () {
-        Route::resource('setoran', SetoranController::class);
-    });
+    // setoran kader
+    Route::resource('setoran', SetoranController::class);
+    
+    // pengguna profile
+    Route::resource('profile', ProfileController::class);    
 });
