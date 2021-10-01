138a6d669a0cb0c2276e38ec834fe540adb7bac9
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-10-01 16:01:04 +0700

    Add delete button

--- resources/views/admin/pengguna/index.blade.php
+++ resources/views/admin/pengguna/index.blade.php
@@ -43,19 +43,22 @@
                         <td>{{ $pengguna->created_at }}</td>
                         <td>{{ $pengguna->updated_at }}</td>
                         <td>
-                            @if ($pengguna->pengguna_profile_status)
-                                <a href="{{ route('admin.pengguna.show', ['pengguna' => $pengguna->id]) }}" class="btn btn-info btn-sm">Detail</a>
+                            @if ($pengguna->pengguna_profile_status && $pengguna->approved_at)
+                                <a href="{{ route('admin.pengguna.show', ['pengguna' => $pengguna->id]) }}" class="btn btn-info btn-sm">Detail</a> 
+                            @elseif ($pengguna->pengguna_profile_status)
+                                <form method="POST" action="{{ route('admin.pengguna.destroy', ['pengguna' => $pengguna->id]) }}">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="DELETE" />
+                                    <a href="{{ route('admin.pengguna.show', ['pengguna' => $pengguna->id]) }}" class="btn btn-info btn-sm">Detail</a>
+                                    <button onclick="event.preventDefault(); this.closest('form').submit();" class="btn btn-danger btn-sm">Hapus</button>
+                                </form>
                             @else 
-
-                            <form method="POST" action="{{ route('admin.pengguna.destroy', ['pengguna' => $pengguna->id]) }}">
-                                @csrf
-                                <input type="hidden" name="_method" value="DELETE" />
-                                <button onclick="event.preventDefault(); this.closest('form').submit();" class="btn btn-danger btn-sm">Hapus</button>
-                            </form>
-
+                                <form method="POST" action="{{ route('admin.pengguna.destroy', ['pengguna' => $pengguna->id]) }}">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="DELETE" />
+                                    <button onclick="event.preventDefault(); this.closest('form').submit();" class="btn btn-danger btn-sm">Hapus</button>
+                                </form>
                             @endif
-
-                            
                         </td>
                     </tr>
                 @endforeach
